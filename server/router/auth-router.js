8607f677da393ce58f8ed2bd2c03767fb842073f
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-11 10:49:23 +0530

    fixing bugs

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/auth-controller');
 
 // Example route for home page
 router.route('/').get(authController.home);
