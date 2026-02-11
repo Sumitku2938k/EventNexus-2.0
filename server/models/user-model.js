1ca3caeef04056f499d1daa636ff1cbe9ac506b2
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-11 20:42:31 +0530

    Creating the User Schema & Model for User Registration

---
+++ server/models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { 
+        type: String, 
+        required: true,
+        trim: true
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 6
+    },
+    role: { 
+        type: String, 
+        enum: ['student', 'admin'], 
+        default: 'student',
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
