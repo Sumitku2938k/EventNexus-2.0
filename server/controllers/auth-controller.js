aee73d9e979fa2299d8205e63aabd229fc6ce2ca
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-13 15:29:02 +0530

    Building a complete user login route with functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user-model');
+const bcrypt = require('bcrypt');
 
 const home = async (req, res) => {
     try {
@@ -29,4 +30,29 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { home, register };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        res.status(200).json({
+            message: 'Login successful',
+            token: await user.generateToken(),
+            userId: user._id.toString()
+        });
+    } catch (error) {
+        console.error('Error in login controller:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+module.exports = { home, register, login };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth-controller');
 
 router.route('/').get(authController.home); //Route for home page
 router.route('/register').post(authController.register); // Route for user registration
+router.route('/login').post(authController.login); // Route for user login
 
 module.exports = router;
