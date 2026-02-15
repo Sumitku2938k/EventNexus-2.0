2793fb8c3434a88121cfd317641be5bf54a514d4
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-15 10:16:10 +0530

    Add routing components and Pages define

---
+++ client/src/pages/CreateEvent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateEvent = () => {
+  return (
+    <div>
+        Create Event Page       
+    </div>
+  )
+}
+
+export default CreateEvent
---
+++ client/src/pages/EventDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventDetail = () => {
+  return (
+    <div>   
+      Event Detail Page
+    </div>
+  )
+}
+
+export default EventDetail
