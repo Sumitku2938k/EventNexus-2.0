4ab6b46635ba9c68571c81a2d9696264f7f6cfb2
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-15 09:41:52 +0530

    Add event validation schemas for creation and update

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const adminController = require('../controllers/admin-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
 const adminMiddleware = require('../middlewares/admin-middleware');
+const { createEventSchema, updateEventSchema } = require('../validator/event-validator');
+const validate = require('../middlewares/validate-middleware');
 
-router.route('/events/create').post(authMiddleware, adminMiddleware, adminController.createEvent); //Route for creating events
+router.route('/events/create').post(authMiddleware, adminMiddleware, validate(createEventSchema), adminController.createEvent); //Route for creating events
 router.route('/events/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteEvent); //Route for deleting events
-router.route('/events/update/:id').patch(authMiddleware, adminMiddleware, adminController.updateEvent); //Route for updating events
+router.route('/events/update/:id').patch(authMiddleware, adminMiddleware, validate(updateEventSchema), adminController.updateEvent); //Route for updating events
 
 module.exports = router;
