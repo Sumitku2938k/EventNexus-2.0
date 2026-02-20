b6b5112bbf1fe92ce06ab3288004d0fc4242e73f
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-20 22:52:27 +0530

    Global Auth State

--- client/src/utils/auth.jsx
+++ client/src/utils/auth.jsx
@@ -3,16 +3,33 @@ import { createContext, useContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
+    //Store token in local storage
     const storeTokenInLS = (serverToken) => {
+        setToken(serverToken);
         localStorage.setItem("token", serverToken);
     };
+    //Store user data in local storage
     const storeUserInLS = (serverUser) => {
+        setUser(serverUser);
         localStorage.setItem("user", JSON.stringify(serverUser));
     };
 
+    let isLoggedIn = !!token; //if token is present then true else false
+    console.log("isLoggedIn : ", isLoggedIn);
+
+    //Tackling the Logout functionality
+    const LogoutUser = () => {
+        setToken("");
+        setUser(null);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+    }
+
     return (
-        <AuthContext.Provider value={{ storeTokenInLS, storeUserInLS }}>
+        <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, storeUserInLS }}>
             {children}
         </AuthContext.Provider>
     );
