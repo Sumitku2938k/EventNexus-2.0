578b2d7d0f7da11f2d22c997f5a438200d38d1f3
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-27 20:21:16 +0530

    Making Event Card and adding basic Events code for listing

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import EventCard from "../components/EventCard";
 
 const Events = () => {
   return (
-    <div>   
-      Events Page
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {events.map((event) => (
+        <EventCard key={event._id} event={event} />
+      ))}
     </div>
   )
 }
