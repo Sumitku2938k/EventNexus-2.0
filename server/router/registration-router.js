b6fe5b98cf8dbf9edc0ddcff3cf002f6fc4d6b14
Sumit Kumar <sumitku2938k@gmail.com>
2026-05-10 11:07:45 +0530

    Add route to check user registration for specific events

--- server/router/registration-router.js
+++ server/router/registration-router.js
@@ -6,4 +6,7 @@ const authMiddleware = require('../middlewares/auth-middleware');
 // Get all registrations for the currently authenticated user
 router.route('/me').get(authMiddleware, registrationController.getMyRegistrations);
 
+// Check if user is registered for a specific event
+router.route('/check/:eventId').get(authMiddleware, registrationController.checkRegistration);
+
 module.exports = router;
